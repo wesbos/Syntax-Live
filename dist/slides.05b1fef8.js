afedbe205f1cbb4c3b79e0bbe2d061763f0008b8
Wes Bos <wesbos@gmail.com>
2020-12-08 13:53:20 -0500

    working

--- dist/slides.05b1fef8.js
+++ dist/slides.05b1fef8.js
@@ -791,7 +791,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62049" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49911" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
